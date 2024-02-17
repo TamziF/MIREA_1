f3922685472a06d3be18addd0a73ce7bcf4d2f3f
Andrew <AndrSh01@yandex.ru>
2024-02-17 12:52:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mirea_1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mirea_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onStart");
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onResume");
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onPause");
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onStop");
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.v("MIREA_1", "onDestroy");
+
+        super.onDestroy();
+    }
+}
